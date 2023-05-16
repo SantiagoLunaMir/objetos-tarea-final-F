cdb31a12b9b770879cbc686172f28ac6e8b5c4c1
Santiago Luna Mireles <a335833@alumnos.uaslp.mx>
2023-05-15 20:25:06 -0600

    Excersie1 listoo

--- src/main/java/uaslp/objetos/figuras/Cuadrado.java
+++ src/main/java/uaslp/objetos/figuras/Cuadrado.java
@@ -3,8 +3,25 @@ package uaslp.objetos.figuras;
 public class Cuadrado {
 
     private double lado;
+    private double area;
+
+    public Cuadrado(double lado) {
+        this.lado=lado;
+    }
+
+    public Cuadrado() {
+
+    }
 
     void setLado(double lado){
         this.lado = lado;
+    }
+
+    public double getArea() {
+        return area=lado*lado;
+    }
+
+    public double getLado() {
+        return lado;
     }
 }
---
+++ src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
@@ -0,0 +1,35 @@
+package uaslp.objetos.figuras;
+
+import static java.lang.Math.sqrt;
+
+public class PoligonoRegular {
+    private int numeroDeLados;
+    private double lado,hipotenusa,ladoChico;
+    private double apotema;
+    private double area;
+
+
+    public PoligonoRegular(int numeroDeLados) {
+        this.numeroDeLados=numeroDeLados;
+    }
+
+    public PoligonoRegular(int numeroDeLados, double lado) {
+        this.numeroDeLados=numeroDeLados;
+        this.lado=lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado=lado;
+    }
+
+    public double getArea() {//sqrt raiz,math.pow(num,2) cuadrado,lado medida indv
+        hipotenusa=Math.pow(lado,2);
+        ladoChico=Math.pow((lado/2),2);
+        apotema=Math.sqrt(hipotenusa-ladoChico);
+        return area=((lado*numeroDeLados)*apotema)/2;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+}
